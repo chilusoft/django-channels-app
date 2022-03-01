bf29a52bfa167980d9aa92ef9082870e13f4ad43
Chilusoft <mukukachilu@gmail.com>
2022-03-01 09:19:40 +0200

    removed SMTP details

--- proj/settings.py
+++ proj/settings.py
@@ -138,8 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email Settings
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'engmukukachilu@gmail.com'
-EMAIL_HOST_PASSWORD = 'techleadhd04'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 10
